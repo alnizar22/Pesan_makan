7a85f8082832e8279703503c6e0f2c9138450c89
alnizar22 <gudangg2209@gmail.com>
2023-07-25 18:56:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/aplikasipesanmakanan/AppCompatActivity.java
@@ -0,0 +1,4 @@
+package com.example.aplikasipesanmakanan;
+
+public class AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/makanan/MenuActivity.java
@@ -0,0 +1,6 @@
+package com.example.makanan;
+
+import android.app.Activity;
+
+public class MenuActivity extends Activity {
+}
